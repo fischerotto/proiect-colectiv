36ef7ddef8dde425d2d7495e73c1ba20679758c7
fischerotto <fischerotto413@icloud.com>
2021-01-05 18:31:03 +0200

    refactor 1

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,9 +7,10 @@ import { SkillAreasComponent } from './skill-areas/skill-areas.component';
 import { ProjectsAreaComponent} from './projects-area/projects-area.component';
 import { TechnologyAreasComponent } from './technology-areas/technology-areas.component';
 import { FormsModule } from '@angular/forms';
+import { RegionAreasComponent } from './region-areas/region-areas.component';
 
 @NgModule({
-  declarations: [DashboardComponent, SkillAreasComponent, ProjectsAreaComponent, TechnologyAreasComponent],
+  declarations: [DashboardComponent, SkillAreasComponent, ProjectsAreaComponent, TechnologyAreasComponent, RegionAreasComponent],
   imports: [CommonModule, AdminRoutingModule, MaterialModule, FormsModule],
 })
 export class AdminModule {}
---
+++ src/app/admin/region-areas/region-areas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-region-areas',
+  templateUrl: './region-areas.component.html',
+  styleUrls: ['./region-areas.component.scss']
+})
+export class RegionAreasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
